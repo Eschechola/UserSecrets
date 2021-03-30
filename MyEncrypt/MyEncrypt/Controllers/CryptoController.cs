eea74caf43c2c517df8fd24b8325570febc5de1c
Eschechola <lucas.eschechola@outlook.com>
2021-03-30 01:40:03 -0300

    Adding projet

---
+++ MyEncrypt/MyEncrypt/Controllers/CryptoController.cs
@@ -0,0 +1,46 @@
+﻿using EscNet.Cryptography.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MyEncrypt.Controllers
+{
+    [Route("api/v1/crypto")]
+    [ApiController]
+    public class CryptoController : ControllerBase
+    {
+        private readonly IRijndaelCryptography _rijndaelCryptography;
+
+        public CryptoController(IRijndaelCryptography rijndaelCryptography)
+        {
+            _rijndaelCryptography = rijndaelCryptography;
+        }
+
+        [HttpPost]
+        [Route("encrypt")]
+        public IActionResult Encrypt([FromBody]string text)
+        {
+            try
+            {
+                return Ok(_rijndaelCryptography.Encrypt(text));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+            }
+        }
+
+        [HttpPost]
+        [Route("decrypt")]
+        public IActionResult Decrypt([FromBody] string text)
+        {
+            try
+            {
+                return Ok(_rijndaelCryptography.Decrypt(text));
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new { message = e.Message });
+            }
+        }
+    }
+}
